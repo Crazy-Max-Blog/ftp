0b7b8a4b7d77298edaa939499d5c4ab47039a5e3
CrazyMax <127091629+Crazy-Max-Blog@users.noreply.github.com>
2023-10-25 22:49:56 +0400

    Update main.py

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ from pyftpdlib.authorizers import DummyAuthorizer
 from pyftpdlib.handlers import FTPHandler
 from pyftpdlib.servers import FTPServer
 authorizer = DummyAuthorizer()
-authorizer.add_user("user", "12345", "/home/runner/work/ftp/ftp/", perm="elradfmwMT")
-authorizer.add_anonymous("/home/runner/work/ftp/ftp/")
+authorizer.add_user("user", "12345", "/", perm="elradfmwMT")
+authorizer.add_anonymous("/")
 handler = FTPHandler
 handler.authorizer = authorizer
 server = FTPServer(("127.0.0.1", 23), handler)
