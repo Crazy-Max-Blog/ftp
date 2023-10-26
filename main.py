7f8692047b7bf6a56b4887ab7b61fcc9fa976334
CrazyMax <127091629+Crazy-Max-Blog@users.noreply.github.com>
2023-10-26 11:10:02 +0400

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
-from pyftpdlib.authorizers import DummyAuthorizer
-from pyftpdlib.handlers import FTPHandler
-from pyftpdlib.servers import FTPServer
-
 import socket
 h_name = socket.gethostname()
 IP_addres = socket.gethostbyname(h_name)
 print("Host Name is:" + h_name)
 print("Computer IP Address is:" + IP_addres)
+
+from pyftpdlib.authorizers import DummyAuthorizer
+from pyftpdlib.handlers import FTPHandler
+from pyftpdlib.servers import FTPServer
 
 authorizer = DummyAuthorizer()
 authorizer.add_user("user", "12345", "/", perm="elradfmwMT")
